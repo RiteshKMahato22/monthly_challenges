6fafd7dd8c6ff4a6be18edc72f0d5b06259a6fd8
Ritesh Mahato <ritesh_mahato@epam.com>
2024-04-14 20:57:11 +0530

    changes

--- challenges/views.py
+++ challenges/views.py
@@ -31,7 +31,7 @@ def monthly_challenge_by_number(request,month):
 
      try:
          redirect_month = months[month - 1]
-         redirect_path=reverse("rev-name",args=[1])
+         redirect_path=reverse("rev-name",args=[redirect_month])
          return HttpResponseRedirect("/challenges/"+redirect_month)
      except:
          return HttpResponseNotFound("Out of range")
